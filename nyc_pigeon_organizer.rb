a37b68e497d472ffec5b2eac1311291a03161a58
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-23 17:13:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+pigeon_1 = {:name => "Theo"} 
+ #pigeon_1 = "Theo"
+pigeon_list = {pigeon_1} 
+binding.pry
+end 
+ 
+ #pigeon_1 = {:name => "Theo"} 
+ #pigeon_2 = {:name => "Peter Jr."}
+ #pigeon_3 = {:name => "Lucky"}
+ #pigeon_4 = {:name => "Ms. K"}
+ #pigeon_5 = {:name => "Queenie"}
+ #pigeon_6 = {:name => "Andrew"}
+ #pigeon_7 = {:name => "Alex"}
+ 
+ #pigeon_1 = [:color, :gender, :lives]
+ #data.collect[:color][:purple] = pigeon_1
+  #pigeon_list = pigeon_1,pigeon_2, pigeon_3, pigeon_4, pigeon_5, pigeon_6, pigeon_7}
+  #pigeon_list
+
