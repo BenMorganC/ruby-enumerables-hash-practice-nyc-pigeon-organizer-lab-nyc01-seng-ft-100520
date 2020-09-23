c35912e7cf7aabebe2788efb58365a9c25816c97
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-23 18:04:39 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,7 +12,9 @@ def nyc_pigeon_organizer(data)
             end
             if !final_array[name][key] 
               final_array[name][key] = []
-            end    
+            end
+          end
+        end
         final_array
     end    
   binding.pry
