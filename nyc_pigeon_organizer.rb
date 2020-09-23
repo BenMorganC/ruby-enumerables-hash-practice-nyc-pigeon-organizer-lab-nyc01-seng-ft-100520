880f64da43c6bd9b0f428bcf2b91a8c88e66c4be
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-23 17:46:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,6 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-#pigeon_list = {} 
     data.each_with_object({}) do |(key,value), final_array|
         value.each do |inner_key, names|  
           binding.pry
